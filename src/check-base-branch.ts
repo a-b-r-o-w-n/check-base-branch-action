9d5cb44fbc86f040dc5f6ef0b652e4c7aca227e1
Andy Brown <asbrown002@gmail.com>
2021-03-05 09:52:57 -0800

    format code

--- src/check-base-branch.ts
+++ src/check-base-branch.ts
@@ -8,8 +8,8 @@ const getPR = () => {
 const getProtectedBranches = (): string[] => {
   const branches = core.getInput("protected-branches", { required: true });
 
-  return branches.split(',').map(b => b.trim());
-}
+  return branches.split(",").map((b) => b.trim());
+};
 
 async function run() {
   const pr = getPR();
@@ -22,31 +22,40 @@ async function run() {
   try {
     const protectedBranches = getProtectedBranches();
     const updateBranch = core.getInput("update-branch");
-    const defaultBranch = core.getInput("default-branch", { required: updateBranch !== 'true' });
-    const token = core.getInput("repo-token", { required: updateBranch !== 'true' });
-
+    const defaultBranch = core.getInput("default-branch", {
+      required: updateBranch !== "true",
+    });
+    const token = core.getInput("repo-token", {
+      required: updateBranch !== "true",
+    });
 
     core.debug(`Checking base branch for PR #${pr.number}`);
 
     if (protectedBranches.includes(pr.base.ref)) {
-      if (updateBranch === 'true') {
-        core.debug(`Updating base branch '${pr.data.base.ref}' to '${defaultBranch}'.`);
+      if (updateBranch === "true") {
+        core.debug(
+          `Updating base branch '${pr.data.base.ref}' to '${defaultBranch}'.`
+        );
 
         const oktokit = new github.GitHub(token);
         const payload = {
           owner: github.context.repo.owner,
           repo: github.context.repo.repo,
-          pull_number: pr.number
+          pull_number: pr.number,
         };
 
         await oktokit.pulls.update({
           ...payload,
-          base: defaultBranch
+          base: defaultBranch,
         });
 
-        core.info(`Branch updated from '${pr.data.base.ref}' to '${defaultBranch}'.`)
+        core.info(
+          `Branch updated from '${pr.data.base.ref}' to '${defaultBranch}'.`
+        );
       } else {
-        core.setFailed(`Base branch set to protected branch '${pr.data.base.ref}'`);
+        core.setFailed(
+          `Base branch set to protected branch '${pr.data.base.ref}'`
+        );
         return;
       }
     } else {
